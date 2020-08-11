ed415c204963096b5e327d9e72a300af290e6829
sebas-94 <sebas.solis.94@gmail.com>
2020-08-11 16:14:05 +0200

    simple version

---
+++ app.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+    // Evento click en 'donwload'
+    $("#btnDonwload").click(function () {
+        // Recoge URL y añade cadena para obtener JSON con metadatos
+        let URL = $('#inputUrl').val() + '?__a=1';
+
+
+        // Recoge JSON
+        $.getJSON(URL, (data) => {
+            // Video o imagen    
+            const type = data.graphql.shortcode_media.__typename;
+            // Obtiene el link (según sea video o no)
+            const link = type === 'GraphVideo' ?
+                data.graphql.shortcode_media.video_url :
+                data.graphql.shortcode_media.display_url;
+
+
+            window.location.href = link;
+           
+        })
+            .fail(() => {
+                alert(`La URL introducida no es correcta.`)
+            })
+
+    });
+
+
+
+});
